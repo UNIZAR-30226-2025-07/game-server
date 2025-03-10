1c4797c9de65aad6cd5f78d1642c8a1f9c5e2564
Hugo Cornago <873840@unizar.es>
2025-03-10 18:37:21 +0100

    world structure + documentation

--- galaxy/world.go
+++ galaxy/world.go
@@ -1,16 +1,27 @@
 package galaxy
 
-// 10000 x 10000
+import (
+	"sync"
 
+	"github.com/google/uuid"
+)
+
+
+// PlayerID is a UUID v4 identifying a unique player.
+// This identifier will be shared with the database.
+type PlayerID uuid.UUID
+
+// Vector2D represents a point in a 2D space.
 type Vector2D struct {
   X uint16
   Y uint16
 }
 
+// World holds all elements inside a current game, this includes players, bots and food.
+// World is locked behind a mutex in order to archieve safe concurrency.
+// Each server should only contain one world at the moment.
 type World struct {
-  /*
-  players
-  food
-  bots
-  */
+  sync.RWMutex
+  players map[uuid.UUID]Player
+  food []Food
 }
