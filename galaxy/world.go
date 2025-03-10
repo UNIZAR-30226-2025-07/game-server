bd712bd4c092971dc2294fc54518904bd02bb285
Hugo Cornago <873840@unizar.es>
2025-03-10 18:51:58 +0100

    add constrains to the world width + height

--- galaxy/world.go
+++ galaxy/world.go
@@ -6,6 +6,10 @@ import (
 	"github.com/google/uuid"
 )
 
+const (
+  WORLD_WIDTH = 10_000;
+  WORLD_HEIGHT = 10_000;
+)
 
 // PlayerID is a UUID v4 identifying a unique player.
 // This identifier will be shared with the database.
