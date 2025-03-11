852706b2776c2d50614f137b113b30292437b420
Hugo Cornago <873840@unizar.es>
2025-03-11 09:35:11 +0100

    flake: add protobuf

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
         in
         {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+            buildInputs = with pkgs; [ go gopls gotools go-tools protobuf ];
             shellHook = ''
               zsh
               exit $?
